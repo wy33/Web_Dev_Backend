af6f921dfce71d341d457bad60752e96657c2b0e
William Yang <yangw031@gmail.com>
2023-11-27 21:50:04 -0500

    Initial API Calls Test, working console.log()s

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const express = require('express')
+// Import express library
+const express = require('express');
+
+// Create express server
 const app = express()
+
+// Port number to listen to
 const port = 3000
 
+// Import defined express routes in "routes" folder
+const routes = require('./routes');
+
+//
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World! (root)')
+});
+
+app.get('/1', (req, res) => {
+    res.send('Hello World! 1 (top level index.js)')
+  });
+
+// Add routes to app
+app.use('', routes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+// Import express
+const express = require('express');
+
+const router = express.Router();
+
+// Import test routes
+const testRouter = require('./test');
+
+// Use test routes, URL "/test/..."
+router.use('/test', testRouter);
+
+// Export router and its routes
+module.exports = router;
